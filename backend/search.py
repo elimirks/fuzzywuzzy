07632d3c6a77d9932b58aafdaf3f66ee5f710f31
123Mpo321 <thempoman@hotmail.com>
2016-01-23 19:00:42 -0500

    Changed search implementation.

--- backend/search.py
+++ backend/search.py
@@ -2,14 +2,21 @@ import re
 from mispellings import *
 from cinnamon import syn
 
+def special_search(needle, haystack, lst):
+    if needle in ["address"] + first_pass("address") + second_pass("address") + syn["address"]:
+        lst.update({(haystack[x.start():x.end()-2], x.start()) for x in re.finditer(' [1-9]+ [a-zA-Z-]+ \W*(?i)(blvd\.|st\.|rd\.|cr\.|ln\.)(?i)\W*( |\?|\.|,|!)', " " + haystack + " ")})
+
 def direct_search(needle, haystack):
-    return {(needle, x.start()) for x in re.finditer(" " + needle + "( |\?|\.|,|!)", " " + haystack + " ")}
+    lst = {}
+    lst.update({(needle, x.start()) for x in re.finditer(" " + needle + "( |\?|\.|,|!)", " " + haystack + " ")})
+    special_search(needle, haystack, lst)
+    return lst
 
 def search(needle, haystack):
     needles = [needle.lower()] + first_pass(needle) + second_pass(needle) + syn[needle.lower()]
-    lst = []
-    for n in needles:lst+=direct_search(n, haystack.lower())
+    lst = {}
+    for n in needles:lst.update(direct_search(n, haystack.lower()))
     return lst
 
 if __name__ == '__main__':
-    print(search("Test", "That test life! direction!"))
+    print(search("address", "That test 95 fifth st. life! direction! 17 Mason St. lool"))
