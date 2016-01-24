40c3f4a521b4ffe580ef39e31ee3f4196468e637
123Mpo321 <thempoman@hotmail.com>
2016-01-23 19:23:59 -0500

    Fixed search.py

--- backend/search.py
+++ backend/search.py
@@ -4,19 +4,18 @@ from cinnamon import syn
 
 def special_search(needle, haystack, lst):
     if needle in ["address"] + first_pass("address") + second_pass("address") + syn["address"]:
-        lst.update({(haystack[x.start():x.end()-2], x.start()) for x in re.finditer(' [1-9]+ [a-zA-Z-]+ \W*(?i)(blvd\.|st\.|rd\.|cr\.|ln\.)(?i)\W*( |\?|\.|,|!)', " " + haystack + " ")})
+        lst += [(haystack[x.start():x.end()-1], x.start()) for x in re.finditer('(^|\W)[1-9]+ [a-zA-Z-]+ \W*(?i)(blvd\.|st\.|rd\.|cr\.|ln\.)(?i)\W*(?=\W|$)', haystack.lower())]
 
 def direct_search(needle, haystack):
-    lst = {}
-    lst.update({(needle, x.start()) for x in re.finditer(" " + needle + "( |\?|\.|,|!)", " " + haystack + " ")})
+    lst = [(haystack[x.start():x.end() + 1], x.start()) for x in re.finditer("(^|\W)" + needle + "(?=\W|$)", haystack.lower())]
     special_search(needle, haystack, lst)
     return lst
 
 def search(needle, haystack):
     needles = [needle.lower()] + first_pass(needle) + second_pass(needle) + syn[needle.lower()]
-    lst = {}
-    for n in needles:lst.update(direct_search(n, haystack.lower()))
+    lst = []
+    for n in needles:lst += (direct_search(n.lower(), haystack))
     return lst
 
 if __name__ == '__main__':
-    print(search("address", "That test 95 fifth st. life! direction! 17 Mason St. lool"))
+    print(search("there", "There there."))
